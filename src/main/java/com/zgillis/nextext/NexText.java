4c4861bcde52e459579d5f3a5d53d198cde711cf
Zachary Gillis <gillisza@uwec.edu>
2018-10-30 15:26:40 -0500

    Initial commit

---
+++ src/main/java/com/zgillis/nextext/NexText.java
@@ -0,0 +1,12 @@
+package com.zgillis.nextext;
+
+public class NexText
+{
+    public static final String APP_VERSION = "0.1";
+
+    public static void main(String[] args)
+    {
+        System.out.println(String.format("NexText Editor - Version %s", APP_VERSION));
+
+    }
+}
---
+++ src/main/java/com/zgillis/nextext/gui/MainWindow.java
@@ -0,0 +1,7 @@
+package com.zgillis.nextext.gui;
+
+import javax.swing.*;
+
+public class MainWindow {
+    private JPanel mainPanel;
+}
