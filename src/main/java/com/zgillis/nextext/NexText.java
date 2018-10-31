8d61647002bbf4ba81fcae856163192ae26eadde
Zachary Gillis <gillisza@uwec.edu>
2018-10-31 11:56:28 -0500

    Basic Window & Menu Structure

--- src/main/java/com/zgillis/nextext/NexText.java
+++ src/main/java/com/zgillis/nextext/NexText.java
@@ -1,5 +1,9 @@
 package com.zgillis.nextext;
 
+import com.zgillis.nextext.gui.MainWindow;
+
+import javax.swing.*;
+
 public class NexText
 {
     public static final String APP_VERSION = "0.1";
@@ -8,5 +12,13 @@ public class NexText
     {
         System.out.println(String.format("NexText Editor - Version %s", APP_VERSION));
 
+//        try {
+//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
     }
 }
--- src/main/java/com/zgillis/nextext/gui/MainWindow.java
+++ src/main/java/com/zgillis/nextext/gui/MainWindow.java
@@ -1,7 +1,66 @@
 package com.zgillis.nextext.gui;
 
-import javax.swing.*;
+import com.zgillis.nextext.NexText;
 
-public class MainWindow {
-    private JPanel mainPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindow extends JFrame
+{
+    // Window Constants
+    private static final String WINDOW_TITLE = "NexText Editor - v" + NexText.APP_VERSION;
+    private static final int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+
+    // Menu Definition
+    JMenuBar menuBar;
+    JMenu fileMenu, editMenu, helpMenu;
+
+    JMenuItem exitMi;
+    JMenuItem cutMi, copyMi, pasteMi, selAllMi;
+    JMenuItem aboutMi, helpMi;
+
+    public MainWindow()
+    {
+        setTitle(WINDOW_TITLE);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        initComponents();
+    }
+
+    private void initComponents()
+    {
+        menuBar = new JMenuBar();
+
+        // FILE MENU
+        fileMenu = new JMenu("File");
+        exitMi = new JMenuItem("Exit");
+        fileMenu.add(exitMi);
+        menuBar.add(fileMenu);
+
+        // EDIT MENU
+        editMenu = new JMenu("Edit");
+        cutMi = new JMenuItem("Cut");
+        copyMi = new JMenuItem("Copy");
+        pasteMi = new JMenuItem("Paste");
+        selAllMi = new JMenuItem("Select All");
+        editMenu.add(cutMi);
+        editMenu.add(copyMi);
+        editMenu.add(pasteMi);
+        editMenu.add(selAllMi);
+        menuBar.add(editMenu);
+
+        // HELP MENU
+        helpMenu = new JMenu("Help");
+        aboutMi = new JMenuItem("About NexText Editor");
+        helpMi = new JMenuItem("Help");
+        helpMenu.add(aboutMi);
+        helpMenu.add(helpMi);
+        menuBar.add(helpMenu);
+
+        setJMenuBar(menuBar);
+    }
 }
